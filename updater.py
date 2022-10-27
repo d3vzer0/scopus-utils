67dae431f19c08513268a5658ca22b45d9a483e1
Joey Dreijer <jdreijer@schubergphilis.com>
2022-10-27 14:02:18 +0200

    Updated dependencies

--- updater.py
+++ updater.py
@@ -1,13 +1,16 @@
-import cli.attck as attck
-import cli.sigma as sigma
-import cli.sentinel as sentinel
+# import cli.attck as attck
+# import cli.sigma as sigma
+# import cli.sentinel as sentinel
+import cli.aws as aws
+
 import typer
 
 app = typer.Typer()
 
-app.add_typer(attck.app, name="attck")
-app.add_typer(sigma.app, name="sigma")
-app.add_typer(sentinel.app, name="sentinel")
+# app.add_typer(attck.app, name="attck")
+# app.add_typer(sigma.app, name="sigma")
+# app.add_typer(sentinel.app, name="sentinel")
+app.add_typer(aws.app, name="aws")
 
 if __name__ == "__main__":
     app()
